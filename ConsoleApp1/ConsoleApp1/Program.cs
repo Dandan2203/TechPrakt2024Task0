9187392975f983ca166bd9c77672438f01c6d49a
Petro Maizenberg <petromaizen@gmail.com>
2024-02-08 07:04:38 +0000

    Завдання 1.8

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num1, num2;
+
+            while (true)
+            {
+                Console.Write("Введіть перше число: ");
+                if (double.TryParse(Console.ReadLine(), out num1))
+                    break;
+                else
+                    Console.WriteLine("Помилка");
+            }
+            while (true)
+            {
+                Console.Write("Введіть друге число: ");
+                if (double.TryParse(Console.ReadLine(), out num2))
+                    break;
+                else
+                    Console.WriteLine("Помилка: Введено некоректне число.");
+            }
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Сума чиселє: {sum}");
+            Console.ReadLine();
+        }
+    }
+}
